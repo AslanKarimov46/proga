7f56cb5fcbe40568887e10398e3276931b3cd877
Aslan Karimov <aslanker18@gmail.com>
2024-10-20 14:28:32 +0400

    20.10

--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -2,17 +2,20 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+template <typename T>
 size_t Matrix<T>::GetRows() const{
 	return data.size();
 }
 
+template <typename T>
 size_t Matrix<T>::GetColumns() const{
 	if(data.empty())
 		return 0;
 	return data[0].size();
 }
 
-void Make_Rectangle(){
+template <typename T>
+void Matrix<T>Make_Rectangle(){
 	int MaxSize;
 	for(const auto& row:data){
 		if(row.size()>MaxSize)
@@ -21,3 +24,21 @@ void Make_Rectangle(){
 	for(auto& row:data)
 		row.resize(MaxSize);
 }
+
+template <typename T>
+T& Matrix<T>::opeartor() (size_t i, size_t j){
+	return data[i][j];
+}
+#endif //MATRIX_HPP
+
+
+
+
+
+
+
+
+
+
+
+
