f03ab0bb636a14ae2a49baf24287e889edfa6fdc
Aslan <aslanker18@gmail.com>
2024-10-21 15:12:48 +0400

    21.10

---
+++ project21_10/cmake-build-debug/CMakeFiles/project21_10.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project21_10.dir/main.cpp.o"
+  "CMakeFiles/project21_10.dir/main.cpp.o.d"
+  "project21_10"
+  "project21_10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project21_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ project21_10/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Logger {
+public:
+    Logger() {
+        std::cout<<"Logger()\n";
+    }
+    ~Logger() {
+        std::cout<<"~Logger()\n";
+    }
+};
+
+int main(){
+    Logger x;
+    std::cout<<"Hello\n";
+}
